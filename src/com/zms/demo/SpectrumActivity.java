d4b32b799adb8903b41597b249a3599fc18222bc
zhoumushui <zhoumushui@gmail.com>
2015-12-25 18:38:51 +0800

    [ZMS]Add spectrum 002.

--- src/com/zms/demo/SpectrumActivity.java
+++ src/com/zms/demo/SpectrumActivity.java
@@ -16,6 +16,9 @@ public class SpectrumActivity extends Activity {
 	private void initialLayout() {
 		ImageLayout spectrum001 = (ImageLayout) findViewById(R.id.spectrum001);
 		spectrum001.init(R.drawable.spectrum_001, "001", "#1E88E5");
+
+		ImageLayout spectrum002 = (ImageLayout) findViewById(R.id.spectrum002);
+		spectrum002.init(R.drawable.spectrum_002, "002", "#1E88E5");
 	}
 
 }
