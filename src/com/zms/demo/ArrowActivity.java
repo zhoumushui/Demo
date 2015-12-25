d5ce11afcef56afb488e7b0ebdb369538dfaca0a
zhoumushui <zhoumushui@gmail.com>
2015-12-25 18:10:04 +0800

    [ZMS]Add arrow type.

---
+++ src/com/zms/demo/ArrowActivity.java
@@ -0,0 +1,21 @@
+package com.zms.demo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ArrowActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_arrow);
+
+		initialLayout();
+	}
+
+	private void initialLayout() {
+		ImageLayout arrow001 = (ImageLayout) findViewById(R.id.arrow001);
+		arrow001.init(R.drawable.arrow_001, "001", "#1E88E5");
+	}
+
+}
