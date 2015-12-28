a7eddfb9d6c8941e731e1c5abe155094ae6e53fe
zhoumushui <zhoumushui@gmail.com>
2015-12-28 14:08:34 +0800

    [ZMS]Add light 002,003.

--- src/com/zms/demo/LightActivity.java
+++ src/com/zms/demo/LightActivity.java
@@ -17,6 +17,14 @@ public class LightActivity extends Activity {
 		ImageLayout light001 = (ImageLayout) findViewById(R.id.light001);
 		light001.init(R.drawable.light_001, "001",
 				getResources().getColor(R.color.fancy_blue));
+
+		ImageLayout light002 = (ImageLayout) findViewById(R.id.light002);
+		light002.init(R.drawable.light_002, "002",
+				getResources().getColor(R.color.fancy_teal));
+
+		ImageLayout light003 = (ImageLayout) findViewById(R.id.light003);
+		light003.init(R.drawable.light_003, "003",
+				getResources().getColor(R.color.fancy_green));
 	}
 
 }
