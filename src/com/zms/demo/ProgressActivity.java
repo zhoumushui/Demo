e614a4132b6618904c6d41a5d56620888c9b2a82
zhoumushui <zhoumushui@gmail.com>
2015-12-28 11:10:34 +0800

    [ZMS]Add progress 011.

--- src/com/zms/demo/ProgressActivity.java
+++ src/com/zms/demo/ProgressActivity.java
@@ -53,6 +53,10 @@ public class ProgressActivity extends Activity {
 		ImageLayout progress010 = (ImageLayout) findViewById(R.id.progress010);
 		progress010.init(R.drawable.progress_010, "010", getResources()
 				.getColor(R.color.fancy_white));
+		
+		ImageLayout progress011 = (ImageLayout) findViewById(R.id.progress011);
+		progress011.init(R.drawable.progress_011, "011", getResources()
+				.getColor(R.color.fancy_teal));
 	}
 
 }
