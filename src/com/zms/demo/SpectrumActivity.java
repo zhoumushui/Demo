befa274d696f82a127c385f7f8dc5a5586f4b2f3
zhoumushui <zhoumushui@gmail.com>
2015-12-29 17:22:55 +0800

    Spectrum>019.

--- src/com/zms/demo/SpectrumActivity.java
+++ src/com/zms/demo/SpectrumActivity.java
@@ -84,6 +84,10 @@ public class SpectrumActivity extends Activity {
 
 		ImageLayout spectrum018 = (ImageLayout) findViewById(R.id.spectrum018);
 		spectrum018.init(R.drawable.spectrum_018, "018", getResources()
+				.getColor(R.color.fancy_yellow));
+		
+		ImageLayout spectrum019 = (ImageLayout) findViewById(R.id.spectrum019);
+		spectrum019.init(R.drawable.spectrum_019, "019", getResources()
 				.getColor(R.color.fancy_white));
 
 	}
