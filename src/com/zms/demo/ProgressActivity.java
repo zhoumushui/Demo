bd4400ff2992ce5c6d9af1b512875fde3750103b
zhoumushui <zhoumushui@gmail.com>
2016-01-07 14:28:14 +0800

    Progress>040.

--- src/com/zms/demo/ProgressActivity.java
+++ src/com/zms/demo/ProgressActivity.java
@@ -165,11 +165,14 @@ public class ProgressActivity extends Activity {
 		ImageLayout progress038 = (ImageLayout) findViewById(R.id.progress038);
 		progress038.init(R.drawable.progress_038, "038", getResources()
 				.getColor(R.color.fancy_white));
-		
+
 		ImageLayout progress039 = (ImageLayout) findViewById(R.id.progress039);
 		progress039.init(R.drawable.progress_039, "039", getResources()
 				.getColor(R.color.fancy_white));
-		
+
+		ImageLayout progress040 = (ImageLayout) findViewById(R.id.progress040);
+		progress040.init(R.drawable.progress_040, "040", getResources()
+				.getColor(R.color.fancy_white));
 
 	}
 
