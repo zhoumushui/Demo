a88307d644f7820afe4a017c3ad0ae4dee51a464
zhoumushui <zhoumushui@gmail.com>
2016-01-29 17:50:50 +0800

    Spectrum>035.

--- src/com/zms/demo/SpectrumActivity.java
+++ src/com/zms/demo/SpectrumActivity.java
@@ -14,141 +14,145 @@ public class SpectrumActivity extends Activity {
 	}
 
 	private void initialLayout() {
-		ImageLayout spectrum001 = (ImageLayout) findViewById(R.id.spectrum001);
-		spectrum001.init(R.drawable.spectrum_001, "001", getResources()
-				.getColor(R.color.fancy_blue));
-
-		ImageLayout spectrum002 = (ImageLayout) findViewById(R.id.spectrum002);
-		spectrum002.init(R.drawable.spectrum_002, "002", getResources()
-				.getColor(R.color.fancy_green));
-
-		ImageLayout spectrum003 = (ImageLayout) findViewById(R.id.spectrum003);
-		spectrum003.init(R.drawable.spectrum_003, "003", getResources()
+		ImageLayout spectrum035 = (ImageLayout) findViewById(R.id.spectrum035);
+		spectrum035.init(R.drawable.spectrum_035, "035", getResources()
 				.getColor(R.color.fancy_white));
 
-		ImageLayout spectrum004 = (ImageLayout) findViewById(R.id.spectrum004);
-		spectrum004.init(R.drawable.spectrum_004, "004", getResources()
+		ImageLayout spectrum034 = (ImageLayout) findViewById(R.id.spectrum034);
+		spectrum034.init(R.drawable.spectrum_034, "034", getResources()
 				.getColor(R.color.fancy_yellow));
 
-		ImageLayout spectrum005 = (ImageLayout) findViewById(R.id.spectrum005);
-		spectrum005.init(R.drawable.spectrum_005, "005", getResources()
+		ImageLayout spectrum033 = (ImageLayout) findViewById(R.id.spectrum033);
+		spectrum033.init(R.drawable.spectrum_033, "033", getResources()
+				.getColor(R.color.fancy_white));
+
+		ImageLayout spectrum032 = (ImageLayout) findViewById(R.id.spectrum032);
+		spectrum032.init(R.drawable.spectrum_032, "032", getResources()
 				.getColor(R.color.fancy_teal));
 
-		ImageLayout spectrum006 = (ImageLayout) findViewById(R.id.spectrum006);
-		spectrum006.init(R.drawable.spectrum_006, "006", getResources()
-				.getColor(R.color.fancy_orange));
-
-		ImageLayout spectrum007 = (ImageLayout) findViewById(R.id.spectrum007);
-		spectrum007.init(R.drawable.spectrum_007, "007", getResources()
-				.getColor(R.color.fancy_yellow));
-
-		ImageLayout spectrum008 = (ImageLayout) findViewById(R.id.spectrum008);
-		spectrum008.init(R.drawable.spectrum_008, "008", getResources()
-				.getColor(R.color.fancy_white));
-
-		ImageLayout spectrum009 = (ImageLayout) findViewById(R.id.spectrum009);
-		spectrum009.init(R.drawable.spectrum_009, "009", getResources()
-				.getColor(R.color.fancy_blue));
-
-		ImageLayout spectrum010 = (ImageLayout) findViewById(R.id.spectrum010);
-		spectrum010.init(R.drawable.spectrum_010, "010", getResources()
-				.getColor(R.color.fancy_green));
-
-		ImageLayout spectrum011 = (ImageLayout) findViewById(R.id.spectrum011);
-		spectrum011.init(R.drawable.spectrum_011, "011", getResources()
-				.getColor(R.color.fancy_white));
-
-		ImageLayout spectrum012 = (ImageLayout) findViewById(R.id.spectrum012);
-		spectrum012.init(R.drawable.spectrum_012, "012", getResources()
-				.getColor(R.color.fancy_blue));
-
-		ImageLayout spectrum013 = (ImageLayout) findViewById(R.id.spectrum013);
-		spectrum013.init(R.drawable.spectrum_013, "013", getResources()
+		ImageLayout spectrum031 = (ImageLayout) findViewById(R.id.spectrum031);
+		spectrum031.init(R.drawable.spectrum_031, "031", getResources()
 				.getColor(R.color.fancy_red));
 
-		ImageLayout spectrum014 = (ImageLayout) findViewById(R.id.spectrum014);
-		spectrum014.init(R.drawable.spectrum_014, "014", getResources()
-				.getColor(R.color.fancy_teal));
+		ImageLayout spectrum030 = (ImageLayout) findViewById(R.id.spectrum030);
+		spectrum030.init(R.drawable.spectrum_030, "030", getResources()
+				.getColor(R.color.fancy_red));
 
-		ImageLayout spectrum015 = (ImageLayout) findViewById(R.id.spectrum015);
-		spectrum015.init(R.drawable.spectrum_015, "015", getResources()
-				.getColor(R.color.fancy_yellow));
-
-		ImageLayout spectrum016 = (ImageLayout) findViewById(R.id.spectrum016);
-		spectrum016.init(R.drawable.spectrum_016, "016", getResources()
-				.getColor(R.color.fancy_orange));
-
-		ImageLayout spectrum017 = (ImageLayout) findViewById(R.id.spectrum017);
-		spectrum017.init(R.drawable.spectrum_017, "017", getResources()
-				.getColor(R.color.fancy_yellow));
-
-		ImageLayout spectrum018 = (ImageLayout) findViewById(R.id.spectrum018);
-		spectrum018.init(R.drawable.spectrum_018, "018", getResources()
-				.getColor(R.color.fancy_yellow));
-
-		ImageLayout spectrum019 = (ImageLayout) findViewById(R.id.spectrum019);
-		spectrum019.init(R.drawable.spectrum_019, "019", getResources()
-				.getColor(R.color.fancy_white));
-
-		ImageLayout spectrum020 = (ImageLayout) findViewById(R.id.spectrum020);
-		spectrum020.init(R.drawable.spectrum_020, "020", getResources()
-				.getColor(R.color.fancy_white));
-
-		ImageLayout spectrum021 = (ImageLayout) findViewById(R.id.spectrum021);
-		spectrum021.init(R.drawable.spectrum_021, "021", getResources()
-				.getColor(R.color.fancy_green));
-
-		ImageLayout spectrum022 = (ImageLayout) findViewById(R.id.spectrum022);
-		spectrum022.init(R.drawable.spectrum_022, "022", getResources()
+		ImageLayout spectrum029 = (ImageLayout) findViewById(R.id.spectrum029);
+		spectrum029.init(R.drawable.spectrum_029, "029", getResources()
 				.getColor(R.color.fancy_blue));
 
-		ImageLayout spectrum023 = (ImageLayout) findViewById(R.id.spectrum023);
-		spectrum023.init(R.drawable.spectrum_023, "023", getResources()
+		ImageLayout spectrum028 = (ImageLayout) findViewById(R.id.spectrum028);
+		spectrum028.init(R.drawable.spectrum_028, "028", getResources()
+				.getColor(R.color.fancy_green));
+
+		ImageLayout spectrum027 = (ImageLayout) findViewById(R.id.spectrum027);
+		spectrum027.init(R.drawable.spectrum_027, "027", getResources()
 				.getColor(R.color.fancy_white));
 
-		ImageLayout spectrum024 = (ImageLayout) findViewById(R.id.spectrum024);
-		spectrum024.init(R.drawable.spectrum_024, "024", getResources()
+		ImageLayout spectrum026 = (ImageLayout) findViewById(R.id.spectrum026);
+		spectrum026.init(R.drawable.spectrum_026, "026", getResources()
 				.getColor(R.color.fancy_white));
 
 		ImageLayout spectrum025 = (ImageLayout) findViewById(R.id.spectrum025);
 		spectrum025.init(R.drawable.spectrum_025, "025", getResources()
 				.getColor(R.color.fancy_red));
 
-		ImageLayout spectrum026 = (ImageLayout) findViewById(R.id.spectrum026);
-		spectrum026.init(R.drawable.spectrum_026, "026", getResources()
+		ImageLayout spectrum024 = (ImageLayout) findViewById(R.id.spectrum024);
+		spectrum024.init(R.drawable.spectrum_024, "024", getResources()
 				.getColor(R.color.fancy_white));
 
-		ImageLayout spectrum027 = (ImageLayout) findViewById(R.id.spectrum027);
-		spectrum027.init(R.drawable.spectrum_027, "027", getResources()
+		ImageLayout spectrum023 = (ImageLayout) findViewById(R.id.spectrum023);
+		spectrum023.init(R.drawable.spectrum_023, "023", getResources()
 				.getColor(R.color.fancy_white));
 
-		ImageLayout spectrum028 = (ImageLayout) findViewById(R.id.spectrum028);
-		spectrum028.init(R.drawable.spectrum_028, "028", getResources()
-				.getColor(R.color.fancy_green));
-
-		ImageLayout spectrum029 = (ImageLayout) findViewById(R.id.spectrum029);
-		spectrum029.init(R.drawable.spectrum_029, "029", getResources()
+		ImageLayout spectrum022 = (ImageLayout) findViewById(R.id.spectrum022);
+		spectrum022.init(R.drawable.spectrum_022, "022", getResources()
 				.getColor(R.color.fancy_blue));
 
-		ImageLayout spectrum030 = (ImageLayout) findViewById(R.id.spectrum030);
-		spectrum030.init(R.drawable.spectrum_030, "030", getResources()
-				.getColor(R.color.fancy_red));
+		ImageLayout spectrum021 = (ImageLayout) findViewById(R.id.spectrum021);
+		spectrum021.init(R.drawable.spectrum_021, "021", getResources()
+				.getColor(R.color.fancy_green));
 
-		ImageLayout spectrum031 = (ImageLayout) findViewById(R.id.spectrum031);
-		spectrum031.init(R.drawable.spectrum_031, "031", getResources()
-				.getColor(R.color.fancy_red));
-
-		ImageLayout spectrum032 = (ImageLayout) findViewById(R.id.spectrum032);
-		spectrum032.init(R.drawable.spectrum_032, "032", getResources()
-				.getColor(R.color.fancy_teal));
-
-		ImageLayout spectrum033 = (ImageLayout) findViewById(R.id.spectrum033);
-		spectrum033.init(R.drawable.spectrum_033, "033", getResources()
+		ImageLayout spectrum020 = (ImageLayout) findViewById(R.id.spectrum020);
+		spectrum020.init(R.drawable.spectrum_020, "020", getResources()
 				.getColor(R.color.fancy_white));
 
-		ImageLayout spectrum034 = (ImageLayout) findViewById(R.id.spectrum034);
-		spectrum034.init(R.drawable.spectrum_034, "034", getResources()
+		ImageLayout spectrum019 = (ImageLayout) findViewById(R.id.spectrum019);
+		spectrum019.init(R.drawable.spectrum_019, "019", getResources()
+				.getColor(R.color.fancy_white));
+
+		ImageLayout spectrum018 = (ImageLayout) findViewById(R.id.spectrum018);
+		spectrum018.init(R.drawable.spectrum_018, "018", getResources()
 				.getColor(R.color.fancy_yellow));
+
+		ImageLayout spectrum017 = (ImageLayout) findViewById(R.id.spectrum017);
+		spectrum017.init(R.drawable.spectrum_017, "017", getResources()
+				.getColor(R.color.fancy_yellow));
+
+		ImageLayout spectrum016 = (ImageLayout) findViewById(R.id.spectrum016);
+		spectrum016.init(R.drawable.spectrum_016, "016", getResources()
+				.getColor(R.color.fancy_orange));
+
+		ImageLayout spectrum015 = (ImageLayout) findViewById(R.id.spectrum015);
+		spectrum015.init(R.drawable.spectrum_015, "015", getResources()
+				.getColor(R.color.fancy_yellow));
+
+		ImageLayout spectrum014 = (ImageLayout) findViewById(R.id.spectrum014);
+		spectrum014.init(R.drawable.spectrum_014, "014", getResources()
+				.getColor(R.color.fancy_teal));
+
+		ImageLayout spectrum013 = (ImageLayout) findViewById(R.id.spectrum013);
+		spectrum013.init(R.drawable.spectrum_013, "013", getResources()
+				.getColor(R.color.fancy_red));
+
+		ImageLayout spectrum012 = (ImageLayout) findViewById(R.id.spectrum012);
+		spectrum012.init(R.drawable.spectrum_012, "012", getResources()
+				.getColor(R.color.fancy_blue));
+
+		ImageLayout spectrum011 = (ImageLayout) findViewById(R.id.spectrum011);
+		spectrum011.init(R.drawable.spectrum_011, "011", getResources()
+				.getColor(R.color.fancy_white));
+
+		ImageLayout spectrum010 = (ImageLayout) findViewById(R.id.spectrum010);
+		spectrum010.init(R.drawable.spectrum_010, "010", getResources()
+				.getColor(R.color.fancy_green));
+
+		ImageLayout spectrum009 = (ImageLayout) findViewById(R.id.spectrum009);
+		spectrum009.init(R.drawable.spectrum_009, "009", getResources()
+				.getColor(R.color.fancy_blue));
+
+		ImageLayout spectrum008 = (ImageLayout) findViewById(R.id.spectrum008);
+		spectrum008.init(R.drawable.spectrum_008, "008", getResources()
+				.getColor(R.color.fancy_white));
+
+		ImageLayout spectrum007 = (ImageLayout) findViewById(R.id.spectrum007);
+		spectrum007.init(R.drawable.spectrum_007, "007", getResources()
+				.getColor(R.color.fancy_yellow));
+
+		ImageLayout spectrum006 = (ImageLayout) findViewById(R.id.spectrum006);
+		spectrum006.init(R.drawable.spectrum_006, "006", getResources()
+				.getColor(R.color.fancy_orange));
+
+		ImageLayout spectrum005 = (ImageLayout) findViewById(R.id.spectrum005);
+		spectrum005.init(R.drawable.spectrum_005, "005", getResources()
+				.getColor(R.color.fancy_teal));
+
+		ImageLayout spectrum004 = (ImageLayout) findViewById(R.id.spectrum004);
+		spectrum004.init(R.drawable.spectrum_004, "004", getResources()
+				.getColor(R.color.fancy_yellow));
+
+		ImageLayout spectrum003 = (ImageLayout) findViewById(R.id.spectrum003);
+		spectrum003.init(R.drawable.spectrum_003, "003", getResources()
+				.getColor(R.color.fancy_white));
+
+		ImageLayout spectrum002 = (ImageLayout) findViewById(R.id.spectrum002);
+		spectrum002.init(R.drawable.spectrum_002, "002", getResources()
+				.getColor(R.color.fancy_green));
+
+		ImageLayout spectrum001 = (ImageLayout) findViewById(R.id.spectrum001);
+		spectrum001.init(R.drawable.spectrum_001, "001", getResources()
+				.getColor(R.color.fancy_blue));
 
 	}
 
